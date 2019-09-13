42bba27854732b0a0fff341a14b8d10c5e369140
navegador <navegador@05000208-70882>
2019-09-13 01:47:04 -0300

    Agrego Bll

---
+++ Business/Safari.Business/ClienteComponent.cs
@@ -0,0 +1,51 @@
+﻿using Safari.Data;
+using Safari.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Business
+{
+    public class ClienteComponent : Component<Cliente>
+    {
+        public override Cliente Create(Cliente objeto)
+        {
+            Cliente result = default(Cliente);
+            var clienteDAC = new ClienteDAC();
+
+            result = clienteDAC.Create(objeto);
+            return result;
+        }
+
+        public override void Delete(int id)
+        {
+            var clienteDAC = new ClienteDAC();
+            clienteDAC.Delete(id);
+        }
+
+        public override List<Cliente> Read()
+        {
+            List<Cliente> result = default(List<Cliente>);
+
+            var clienteDAC = new ClienteDAC();
+            result = clienteDAC.Read();
+            return result;
+        }
+
+        public override Cliente ReadBy(int id)
+        {
+            Cliente result = default(Cliente);
+            var clienteDAC = new ClienteDAC();
+            result = clienteDAC.ReadBy(id);
+            return result;
+        }
+
+        public override void Update(Cliente objeto)
+        {
+            var clienteDAC = new ClienteDAC();
+            clienteDAC.Update(objeto);
+        }
+    }
+}
---
+++ Business/Safari.Business/TipoMovimientoComponent.cs
@@ -0,0 +1,51 @@
+﻿using Safari.Data;
+using Safari.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Business
+{
+    public class TipoMovimientoComponent : Component<TipoMovimiento>
+    {
+        public override TipoMovimiento Create(TipoMovimiento objeto)
+        {
+            TipoMovimiento result = default(TipoMovimiento);
+            var tipoMoviemtoDAC = new TipoMovimientoDAC();
+
+            result = tipoMoviemtoDAC.Create(objeto);
+            return result;
+        }
+
+        public override void Delete(int id)
+        {
+            var tipoMoviemtoDAC = new TipoMovimientoDAC();
+            tipoMoviemtoDAC.Delete(id);
+        }
+
+        public override List<TipoMovimiento> Read()
+        {
+            List<TipoMovimiento> result = default(List<TipoMovimiento>);
+
+            var tipoMovimientoDAC = new TipoMovimientoDAC();
+            result = tipoMovimientoDAC.Read();
+            return result;
+        }
+
+        public override TipoMovimiento ReadBy(int id)
+        {
+            TipoMovimiento result = default(TipoMovimiento);
+            var tipoMovimientoDAC = new TipoMovimientoDAC();
+            result = tipoMovimientoDAC.ReadBy(id);
+            return result;
+        }
+
+        public override void Update(TipoMovimiento objeto)
+        {
+            var tipoMovimientoDAC = new TipoMovimientoDAC();
+            tipoMovimientoDAC.Update(objeto);
+        }
+    }
+}

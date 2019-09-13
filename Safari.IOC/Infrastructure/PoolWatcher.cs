d0046e4ded15ec06415beaefcc7cc31220f4f2f7
navegador <navegador@05000208-70882>
2019-09-13 03:59:51 -0300

    IOC parte 1

---
+++ Safari.IOC/Infrastructure/PoolWatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Safari.Infranstructure
+{
+    public class PoolWatcher
+    {
+        private INotificationAction _action;
+        public PoolWatcher() { }
+        public PoolWatcher(INotificationAction notificatioAction)
+
+        {
+            _action = notificatioAction;
+        }
+        public INotificationAction Action
+
+        {
+            get
+            {
+                return Action;
+            }
+            set
+            {
+                _action = value;
+            }
+
+
+           
+        }
+
+        public void Notify(String message)
+
+        {
+            if (_action==null)
+            {
+                _action = new EventLogWritter();
+
+            }
+        }
+
+
+    }
+}

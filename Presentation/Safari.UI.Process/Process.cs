c9c73d88c452b397ec358f04f8efb1f8decfe57e
Andres Fernando <elmonito007@hotmail.com>
2019-09-23 20:57:25 -0300

    Genero interfaz generics para process

---
+++ Presentation/Safari.UI.Process/Process.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.UI.Process
+{
+public   interface Process<T>
+    {
+          List<T> ListarTodos();
+          T Agregar(T objeto);
+       T Ver(int id);
+        T Eliminar(int id);
+        T Editar(int id, T objeto);
+
+
+
+    }
+}
---
+++ Presentation/Safari.UI.Process/TipoServicioProcess.cs
@@ -0,0 +1,40 @@
+﻿using Safari.Entities;
+using Safari.Services;
+using Safari.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.UI.Process
+{
+    public class TipoServicioProcess : ProcessComponent, Process<TipoServicio>
+    {
+        public TipoServicio Agregar(TipoServicio objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoServicio Editar(int id, TipoServicio objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoServicio Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoServicio> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TipoServicio Ver(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

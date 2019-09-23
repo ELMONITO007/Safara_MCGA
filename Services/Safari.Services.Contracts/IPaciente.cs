9754812f84a78eed0441b9866208f5944f498b6f
Andres Fernando <elmonito007@hotmail.com>
2019-09-23 20:02:48 -0300

    Se agregan los contratos

---
+++ Services/Safari.Services.Contracts/IPaciente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Safari.Entities;
+namespace Safari.Services.Contracts
+{
+ public   interface IPaciente : Contracts<Paciente>
+    {
+    }
+}
